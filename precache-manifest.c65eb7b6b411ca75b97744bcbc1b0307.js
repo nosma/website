82c258fcc1a27c65e11f686788e8827d440a3eb9
MANOS FRAGKAKIS <manos@manoss-mbp.mynet>
2020-04-26 02:25:55 +0100

    Updates

---
+++ precache-manifest.c65eb7b6b411ca75b97744bcbc1b0307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d33db53b11fcc174089135fc5ccef3",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "61ae8db374104c955495",
+    "url": "/website/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "d02a8d293dbfee2196d2",
+    "url": "/website/static/js/2.165bcaf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.165bcaf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ae8db374104c955495",
+    "url": "/website/static/js/main.ab4fc1d8.chunk.js"
+  },
+  {
+    "revision": "a8b244334faffe2d8750",
+    "url": "/website/static/js/runtime-main.da8521c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
